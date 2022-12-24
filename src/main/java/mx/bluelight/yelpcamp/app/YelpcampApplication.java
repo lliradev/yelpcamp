7980ca7bc642281f56eea1e881d61d533207effe
Luis Lira <luislira1029@gmail.com>
2022-12-23 21:39:07 -0600

    Initial commit

---
+++ src/main/java/mx/bluelight/yelpcamp/app/YelpcampApplication.java
@@ -0,0 +1,13 @@
+package mx.bluelight.yelpcamp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/bluelight/yelpcamp/app/YelpcampApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.bluelight.yelpcamp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YelpcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
