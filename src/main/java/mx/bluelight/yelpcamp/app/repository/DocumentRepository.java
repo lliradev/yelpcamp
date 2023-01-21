2b36c78bbb7429e168dd9df89927ed7d321719e2
Luis Lira <luislira1029@gmail.com>
2023-01-20 21:10:35 -0600

    Delete innecesary class and configuration

--- src/main/java/mx/bluelight/yelpcamp/app/repository/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package mx.bluelight.yelpcamp.app.repository;
-
-import mx.bluelight.yelpcamp.app.domain.Authorization;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface DocumentRepository {
-
-    void copyLocalToRemote(Authorization authorization, MultipartFile document);
-}
